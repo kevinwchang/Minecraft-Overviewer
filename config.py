154786d84c8d56a0010994db98305ca93b35c6a5
Kevin Chang <kevin@iprefermuffins.com>
2012-03-04 12:29:00 -0800

    1.2 settings changes

---
+++ config.py
@@ -0,0 +1,23 @@
+worlds['world'] = 'world'
+
+outputdir = '/var/www/minecraft/map'
+
+renders['day'] = {
+	'world': 'world',
+	'title': 'Day',
+	'rendermode': 'smooth_lighting',
+	'dimension': 'overworld',
+	'northdirection': 'upper-right',
+}
+
+renders['night'] = {
+	'world': 'world',
+	'title': 'Night',
+	'rendermode': 'smooth_night',
+	'dimension': 'overworld',
+	'northdirection': 'upper-right',
+}
+
+imgformat = 'jpg'
+imgquality = 50
+processes = 3
--- settings.py
+++
@@ -1,5 +0,0 @@
-rendermode = ['smooth-lighting', 'smooth-night']
-north_direction = 'upper-left'
-imgformat = 'jpg'
-imgquality = 50
-procs = 3
