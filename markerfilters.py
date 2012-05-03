41e9c66244ee97f444e16aa9c3a15bca071f6469
Kevin Chang <kevin@iprefermuffins.com>
2012-05-03 01:24:13 -0700

    more config updates

--- markerfilters.py
+++ markerfilters.py
@@ -1,5 +1,7 @@
 import re
 
+# helpers
+
 def fulltext(poi):
 	return poi['Text1'] + poi['Text2'] + poi['Text3'] + poi['Text4']
 
@@ -8,13 +10,19 @@ label_re = re.compile('^\s*#')
 def islabelsign(poi):
 	return label_re.search(fulltext(poi)) is not None
 
+# filters
+
 def labelsign(poi):
-	return poi['id'] == 'Sign' and islabelsign(poi)
+	if poi['id'] == 'Sign' and islabelsign(poi):
+		return fulltext(poi)
 
 def normalsign(poi):
-	return poi['id'] == 'Sign' and not islabelsign(poi) and not fulltext(poi).strip() == ''
+	if poi['id'] == 'Sign' and not islabelsign(poi) and not fulltext(poi).strip() == '':
+		return fulltext(poi)
+
+isbot_re = re.compile('bot$')
 
 def playericons(poi):
-	if poi['id'] == 'Player':
+	if poi['id'] == 'Player' and isbot_re.search(poi['EntityId']) is None:
 		poi['icon'] = "http://overviewer.org/avatar/%s" % poi['EntityId']
 		return "Last known location for %s" % poi['EntityId']
