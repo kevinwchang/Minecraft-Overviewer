b2d2c3cdf9b41dc4d0ffaa8ab99c85512a6580b1
Kevin Chang <kevin@kevinwchang.com>
2015-12-04 01:45:52 -0800

    tabs to spaces; enable end and logprint players

--- config.py
+++ config.py
@@ -15,59 +15,59 @@ end_ambient_smooth_lighting = [Base(), EdgeLines(), SmoothLighting(strength=0.8)
 nether_ambient_smooth_lighting = [Base(), EdgeLines(), Nether(), SmoothLighting(strength=0.8)]
 
 filters = [
-	dict(name = 'Labels',  filterFunction = markerfilters.labelsign, checked = True),
-	dict(name = 'Signs',   filterFunction = markerfilters.normalsign),
-	dict(name = 'Players', filterFunction = markerfilters.playericons)]
+  dict(name = 'Labels',  filterFunction = markerfilters.labelsign, checked = True),
+  dict(name = 'Signs',   filterFunction = markerfilters.normalsign),
+  dict(name = 'Players', filterFunction = markerfilters.playericons)]
 
 texturepath='~/.minecraft/versions/' + os.getenv('VERSION') + '/' + os.getenv('VERSION') + '.jar'
 
 renders['day'] = {
-	'world': 'world',
-	'title': 'Day',
-	'rendermode': 'smooth_lighting',
-	'dimension': 'overworld',
-	'northdirection': 'upper-right',
-	'markers': filters,
-	'poititle': 'Markers'
+  'world': 'world',
+  'title': 'Day',
+  'rendermode': 'smooth_lighting',
+  'dimension': 'overworld',
+  'northdirection': 'upper-right',
+  'markers': filters,
+  'poititle': 'Markers'
 }
 
 renders['night'] = {
-	'world': 'world',
-	'title': 'Night',
-	'rendermode': 'smooth_night',
-	'dimension': 'overworld',
-	'northdirection': 'upper-right',
-	'markers': filters,
-	'poititle': 'Markers'
+  'world': 'world',
+  'title': 'Night',
+  'rendermode': 'smooth_night',
+  'dimension': 'overworld',
+  'northdirection': 'upper-right',
+  'markers': filters,
+  'poititle': 'Markers'
 }
 
 renders['biomeover'] = {
-	'world': 'world',
-	'title': 'Biomes',
-	'rendermode': [ClearBase(), BiomeOverlay()],
-	'dimension': 'overworld',
-	'northdirection': 'upper-right',
-	'overlay': ['day', 'night'],
-	'imgformat': 'png',
-	'optimizeimg': [optipng()]
+  'world': 'world',
+  'title': 'Biomes',
+  'rendermode': [ClearBase(), BiomeOverlay()],
+  'dimension': 'overworld',
+  'northdirection': 'upper-right',
+  'overlay': ['day', 'night'],
+  'imgformat': 'png',
+  'optimizeimg': [optipng()]
 }
 
-#renders['end'] = {
-#	'world': 'world',
-#	'title': 'End',
-#	'rendermode': end_ambient_smooth_lighting,
-#	'dimension': 'end',
-#	'northdirection': 'upper-right',
-#	'markers': filters,
-#	'poititle': 'Markers'
-#}
+renders['end'] = {
+  'world': 'world',
+  'title': 'End',
+  'rendermode': end_ambient_smooth_lighting,
+  'dimension': 'end',
+  'northdirection': 'upper-right',
+  'markers': filters,
+  'poititle': 'Markers'
+}
 
 renders['nether'] = {
-	'world': 'world',
-	'title': 'Nether',
-	'rendermode': nether_ambient_smooth_lighting,
-	'dimension': 'nether',
-	'northdirection': 'upper-right',
-	'markers': filters,
-	'poititle': 'Markers'
+  'world': 'world',
+  'title': 'Nether',
+  'rendermode': nether_ambient_smooth_lighting,
+  'dimension': 'nether',
+  'northdirection': 'upper-right',
+  'markers': filters,
+  'poititle': 'Markers'
 }
--- markerfilters.py
+++ markerfilters.py
@@ -4,37 +4,38 @@ import time
 # helpers
 
 def fulltext(poi):
-	raw = '\n'.join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
-	decoded = raw.replace(u'\uf700', '').replace(u'\uf701', '').decode('unicode-escape')
-	return raw, decoded
+  raw = '\n'.join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+  decoded = raw.replace(u'\uf700', '').replace(u'\uf701', '').decode('unicode-escape')
+  return raw, decoded
 
 label_re = re.compile('^\s*#')
 
 def islabelsign(text):
-	return label_re.search(text) is not None
+  return label_re.search(text) is not None
 
 fakeplayer_re = re.compile('(bot|cam)$', re.I)
 
 def isrealplayer(poi):
-	return fakeplayer_re.search(poi['EntityId']) is None
+  return fakeplayer_re.search(poi['EntityId']) is None
 
 # filters
 
 def labelsign(poi):
-	if poi['id'] == 'Sign':
-		raw, decoded = fulltext(poi)
-		if islabelsign(decoded):
-			print 'Label: ' + decoded.replace('\n', ' ') + ' (raw: ' + repr(raw) + ')'
-			return decoded
+  if poi['id'] == 'Sign':
+    raw, decoded = fulltext(poi)
+    if islabelsign(decoded):
+      print 'Label: ' + decoded.replace('\n', ' ') + ' (raw: ' + repr(raw) + ')'
+      return decoded
 
 def normalsign(poi):
-	if poi['id'] == 'Sign':
-		raw, decoded = fulltext(poi)
-		if not islabelsign(decoded) and not decoded.strip() == '':
-			print 'Sign: ' + decoded.replace('\n', ' ') + ' (raw: ' + repr(raw) + ')'
-			return decoded
+  if poi['id'] == 'Sign':
+    raw, decoded = fulltext(poi)
+    if not islabelsign(decoded) and not decoded.strip() == '':
+      print 'Sign: ' + decoded.replace('\n', ' ') + ' (raw: ' + repr(raw) + ')'
+      return decoded
 
 def playericons(poi):
-	if poi['id'] == 'Player' and isrealplayer(poi):
-		poi['icon'] = "http://overviewer.org/avatar/%s" % poi['EntityId']
-		return 'Last known location for {0}\n{1}'.format(poi['EntityId'], time.strftime('%A, %B %d, %Y\n%H:%M:%S %Z', poi['time']))
+  if poi['id'] == 'Player' and isrealplayer(poi):
+    print 'Player: ' + poi['EntityId']
+    poi['icon'] = "http://overviewer.org/avatar/%s" % poi['EntityId']
+    return 'Last known location for {0}\n{1}'.format(poi['EntityId'], time.strftime('%A, %B %d, %Y\n%H:%M:%S %Z', poi['time']))
