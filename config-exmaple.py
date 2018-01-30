80dcd4263d2c9ec74f7800e3c562a8731220b295
Kevin Chang <kevin@kevinwchang.com>
2018-01-29 23:28:24 -0800

    exmaple config update

--- config-exmaple.py
+++ config-exmaple.py
@@ -1,24 +1,13 @@
 import markerfilters
+from optimizeimages import optipng
 
 worlds['world'] = 'Minecraft-Overviewer-Addons/exmaple'
 
-outputdir = '/var/www/minecraft/exmaple'
+outputdir = 'output/exmaple'
 
-filters = [
-	dict(name = 'Labels',  filterFunction = markerfilters.labelsign),
-	dict(name = 'Signs',   filterFunction = markerfilters.normalsign),
-	dict(name = 'Players', filterFunction = markerfilters.playericons)]
+filters = []
 
-renders['UL'] = {
-	'world': 'world',
-	'title': 'Upper Left',
-	'rendermode': 'normal',
-	'dimension': 'overworld',
-	'northdirection': 'upper-left',
-	'markers': filters,
-	'poititle': 'Markers'
-}
-renders['UR'] = {
+renders['day'] = {
 	'world': 'world',
 	'title': 'Upper Right',
 	'rendermode': 'normal',
@@ -27,23 +16,16 @@ renders['UR'] = {
 	'markers': filters,
 	'poititle': 'Markers'
 }
-renders['LL'] = {
-	'world': 'world',
-	'title': 'Lower Left',
-	'rendermode': 'normal',
-	'dimension': 'overworld',
-	'northdirection': 'lower-left',
-	'markers': filters,
-	'poititle': 'Markers'
-}
-renders['LR'] = {
-	'world': 'world',
-	'title': 'Lower Right',
-	'rendermode': 'normal',
-	'dimension': 'overworld',
-	'northdirection': 'lower-right',
-	'markers': filters,
-	'poititle': 'Markers'
+
+renders['spawnover'] = {
+  'world': 'world',
+  'title': 'Spawn',
+  'rendermode': [ClearBase(), SpawnOverlay()],
+  'dimension': 'overworld',
+  'northdirection': 'upper-right',
+  'overlay': ['day'],
+  'imgformat': 'png',
+  'optimizeimg': [optipng()]
 }
 
 imgformat = 'jpg'
